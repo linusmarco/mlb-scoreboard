e05a15303e3e23f05b8b1d19ab11d52fe3d2356a
Linus Marco <lmarco163@gmail.com>
2017-02-26 01:00:23 -0500

    getting linescore and scoreboard components up and running with data

--- src/client/app/scoreboard/scoreboard.component.ts
+++ src/client/app/scoreboard/scoreboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute }   from '@angular/router';
+
 import { GamesService } from "../services/games.service";
 
 @Component({
@@ -8,15 +10,29 @@ import { GamesService } from "../services/games.service";
 })
 
 export class ScoreboardComponent implements OnInit {
-    data: any[];
+    date: any;
+    games: any[];
     ready: boolean = false;
 
-    constructor(private _gamesService: GamesService) { }
+    constructor(private _gamesService: GamesService,
+                private _route: ActivatedRoute) { }
 
     ngOnInit() {
-        this._gamesService.getGamesByDate("18710504").then(d => {
-            this.data = d;
-            this.ready = true;
+        let subscr = this._route.params.subscribe(params => {
+            let getDate;
+            let reqDate = params['date'];
+            if (reqDate === "today") {
+                getDate = "20160404";
+            } 
+            else {
+                getDate = reqDate;
+            }
+            this.date = this._gamesService.parseDate(getDate);
+            this._gamesService.getGamesByDate(getDate).then(d => {
+                this.games = d;
+                this.ready = true;
+                subscr.unsubscribe();
+            });
         });
     }
 }
